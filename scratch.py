36cacf266251a8f20c3821625fe343359de6768a
Esteban <laplansk@gmail.com>
2015-01-21 15:21:31 -0800

    sanity commit

--- scratch.py
+++ scratch.py
@@ -6,8 +6,9 @@ import sys
 from threading import Thread
 from time import sleep
 import random
+import struct
 
-#Python's weird way of subclassing...
+# Python's weird way of subclassing...
 class InputListener(Thread):
     def __init__(self):
         super(InputListener, self).__init__()
@@ -31,37 +32,38 @@ GOODBYE = 3
 # server connection variables - these should not change
 IP = "127.0.0.1"
 PORT = 3030
-MAGIC1 = 196
-MAGIC2 = 97
+MAGIC = 0xC461
 VERSION = 1
 SESSION_ID = random.getrandbits(32)
 SequenceNo = 0
 
 def responseListen():
     # TODO define listening behavior
-while True:
-    try:
-        response = client.recv(1024)
-    except socket.timeout, e:
-        if e.args[0] == errno.EAGAIN or e.args[0] == errno.EWOULDBLOCK:
-            continue;
-        elif e.args[0] == 'timed out':
-            sendGoodbye()
-        elif response == "HELLO":
-            # TODO fire up listener and start listening for responses
-            inputListener = InputListener()
-            inputListener.start()
-            listen()
-        else:
-            print "error"
-            sys.exit(1)
+    while True:
+        try:
+            response = client.recv(1024)
+             # unpack the struct and
+        except socket.timeout, e:
+            if e.args[0] == errno.EAGAIN or e.args[0] == errno.EWOULDBLOCK:
+                continue;
+            elif e.args[0] == 'timed out':
+                sendGoodbye()
+            elif response == "HELLO":
+                # TODO fire up listener and start listening for responses
+                inputListener = InputListener()
+                inputListener.start()
+                listen()
+            else:
+                print "error"
+                sys.exit(1)
 
 def sendGoodbye():
     # TODO send GOODBYE MESSAGE AND WAIT FOR RESPONSE BEFORE EXITING
     # create goodbye header
-    headerArray = {MAGIC1, MAGIC2, VERSION, GOODBYE}
-    headerBytes = bytearray(headerArray)
-    goodbyeMessage = str(headerBytes) + str(SequenceNo) + str(SESSION_ID)
+    # headerArray = {MAGIC1, MAGIC2, VERSION, GOODBYE}
+    # headerBytes = bytearray(headerArray)
+    # goodbyeMessage = str(headerBytes) + str(SequenceNo) + str(SESSION_ID)
+    goodbyeMessage = struct.pack('H2B2I', MAGIC, 1, GOODBYE, SequenceNo, SESSION_ID)
     client.sendto(goodbyeMessage, (IP, PORT))
 
     # wait 5 seconds for a GOODBYE response before exiting
@@ -72,7 +74,7 @@ def sendGoodbye():
             response = client.recv(1024)
         except socket.timeout, e:
             if e.args[0] == errno.EAGAIN or e.args[0] == errno.EWOULDBLOCK:
-                continue;
+                continue
             elif e.args[0] == 'timed out':
                 print 'timeout on goodbye'
                 sys.exit(0)
@@ -84,6 +86,9 @@ def sendGoodbye():
                 sys.exit(1)
 
 
+
+helloMessage = struct.pack('H2B2I', MAGIC, 1, HELLO, SequenceNo, SESSION_ID)
+
 # establish connection with server
 # if successful, launch listener
 # else exit?
@@ -91,10 +96,10 @@ client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # assemble the header bytes:
 # 1. input header values
 # 2. reduce necessary values to single bytes
-headerArray = {MAGIC1, MAGIC2, VERSION, HELLO}
-headerBytes = bytearray(headerArray)
-fullHeader = str(headerBytes) + str(SequenceNo) + str(SESSION_ID)
-client.sendto(fullHeader, (IP, PORT))
+# headerArray = {MAGIC1, MAGIC2, VERSION, HELLO}
+# headerBytes = bytearray(headerArray)
+# fullHeader = str(headerBytes) + str(SequenceNo) + str(SESSION_ID)
+client.sendto(helloMessage, (IP, PORT))
 client.settimeout(5)
 SequenceNo += 1
 
@@ -104,7 +109,7 @@ while True:
         response = client.recv(1024)
     except socket.timeout, e:
         if e.args[0] == errno.EAGAIN or e.args[0] == errno.EWOULDBLOCK:
-            continue;
+            continue
         elif e.args[0] == 'timed out':
             sendGoodbye()
         elif response == "HELLO":
